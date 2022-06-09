68a8d17deb061ee47bcc859dcf16ce5d6fb052cb
peroxy <peroxy@users.noreply.github.com>
2022-06-09 09:54:46 +0200

    Add scope IDL

--- packages/hubble-idl/src/index.ts
+++ packages/hubble-idl/src/index.ts
@@ -1,6 +1,12 @@
 import BorrowingIdl from './borrowing.json';
+import ScopeIdl from './scope.json';
 
 /**
  * Hubble Borrowing IDL
  */
 export const BORROWING_IDL = BorrowingIdl;
+
+/**
+ * Scope IDL
+ */
+export const SCOPE_IDL = ScopeIdl;
--- packages/hubble-idl/tests/idl.test.ts
+++ packages/hubble-idl/tests/idl.test.ts
@@ -1,4 +1,5 @@
 import { BORROWING_IDL } from '../dist';
+import { SCOPE_IDL } from '../src';
 
 describe('IDL Tests', () => {
   test('should return Hubble borrowing IDL ', () => {
@@ -8,5 +9,14 @@ describe('IDL Tests', () => {
     expect(BORROWING_IDL.accounts.length).toBeGreaterThan(0);
     expect(BORROWING_IDL.types.length).toBeGreaterThan(0);
     expect(BORROWING_IDL.instructions.length).toBeGreaterThan(0);
+  });
+
+  test('should return Scope IDL ', () => {
+    expect(SCOPE_IDL).not.toBeNull();
+    expect(SCOPE_IDL.name).toBe('scope');
+    expect(SCOPE_IDL.errors.length).toBeGreaterThan(0);
+    expect(SCOPE_IDL.accounts.length).toBeGreaterThan(0);
+    expect(SCOPE_IDL.types.length).toBeGreaterThan(0);
+    expect(SCOPE_IDL.instructions.length).toBeGreaterThan(0);
   });
 });
