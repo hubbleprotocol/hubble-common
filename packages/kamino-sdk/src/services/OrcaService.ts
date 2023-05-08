f7cd208fa7802ace5a9eb1c4e684203681d70368
Silviu Troscot <silviu.troscot10@gmail.com>
2023-05-09 00:48:26 +0300

    wip

--- packages/kamino-sdk/src/services/OrcaService.ts
+++ packages/kamino-sdk/src/services/OrcaService.ts
@@ -6,9 +6,11 @@ import {
   OrcaWhirlpoolClient,
   getNearestValidTickIndexFromTickIndex,
   priceToTickIndex,
+  PDAUtil,
   PoolData,
   TickUtil,
   TICK_ARRAY_SIZE,
+  WhirlpoolContext,
 } from '@orca-so/whirlpool-sdk';
 import axios from 'axios';
 import { OrcaWhirlpoolsResponse, Whirlpool } from './OrcaWhirlpoolsResponse';
@@ -19,6 +21,7 @@ import { Position } from '../whirpools-client';
 import { getKaminoTokenName, getScopeTokenFromKaminoMints } from '../constants';
 import { WhirlpoolAprApy } from './WhirlpoolAprApy';
 import { aprToApy, getStrategyPriceRangeOrca, ZERO } from '../utils';
+import { WHIRLPOOL_PROGRAM_ID } from '../whirpools-client/programId';
 
 export class OrcaService {
   private readonly _connection: Connection;
@@ -56,12 +59,12 @@ export class OrcaService {
     const TICKARRAY_LOWER_OFFSET = -3;
     const TICKARRAY_UPPER_OFFSET = +3;
     for (let offset = TICKARRAY_LOWER_OFFSET; offset <= TICKARRAY_UPPER_OFFSET; offset++) {
-      const start_tick_index = TickUtil.getStartTickIndex(whirlpool_data.tickCurrentIndex, tick_spacing, offset);
+      const start_tick_index = TickUtil.getStartTickIndex(poolData.tickCurrentIndex, poolData.tick_spacing, offset);
       const pda = PDAUtil.getTickArrayFromTickIndex(
         start_tick_index,
-        tick_spacing,
-        whirlpool_key,
-        ORCA_WHIRLPOOL_PROGRAM_ID
+        poolData.tick_spacing,
+        poolPk,
+        WHIRLPOOL_PROGRAM_ID
       );
       tickarray_start_indexes.push(start_tick_index);
       tickarray_pubkeys.push(pda.publicKey);
@@ -69,6 +72,30 @@ export class OrcaService {
 
     // get tickarrays
     const tickarrays = await ctx.fetcher.listTickArrays(tickarray_pubkeys, true);
+
+    // sweep liquidity
+    const current_initializable_tick_index = Math.floor(poolData.tickCurrentIndex / tick_spacing) * tick_spacing;
+    const current_pool_liquidity = whirlpool_data.liquidity;
+    const liquidity_distribution = [];
+    let liquidity = new BN(0);
+    let liquidity_difference;
+    for (let ta = 0; ta < tickarrays.length; ta++) {
+      const tickarray = tickarrays[ta];
+
+      for (let i = 0; i < TICK_ARRAY_SIZE; i++) {
+        const tick_index = tickarray_start_indexes[ta] + i * tick_spacing;
+
+        // move right (add liquidityNet)
+        liquidity = tickarray == null ? liquidity : liquidity.add(tickarray.ticks[i].liquidityNet);
+
+        liquidity_distribution.push({ tick_index, liquidity });
+
+        // liquidity in TickArray not read
+        if (tick_index === current_initializable_tick_index) {
+          liquidity_difference = current_pool_liquidity.sub(liquidity);
+        }
+      }
+    }
   }
 
   private getTokenPrices(strategy: WhirlpoolStrategy, prices: ScopeToken[]) {
