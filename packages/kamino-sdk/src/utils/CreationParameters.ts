9b0a5c27b1a3f47f61c2ab7a83f0a822579c2601
Silviu Troscot <silviu.troscot10@gmail.com>
2023-05-29 21:18:12 +0300

    use usdc instead of usdh in diy vaults default config (#259)

--- packages/kamino-sdk/src/utils/CreationParameters.ts
+++ packages/kamino-sdk/src/utils/CreationParameters.ts
@@ -13,8 +13,8 @@ export const DefaultLowerPriceDifferenceBPS: number = 500;
 export const DefaultUpperPriceDifferenceBPS: number = 500;
 
 export const DefaultMintTokenA: PublicKey = new PublicKey('So11111111111111111111111111111111111111112');
-export const DefaultMintTokenB: PublicKey = new PublicKey('USDH1SM1ojwWUga67PGrgFWUHibbjqMvuMaDkRJTgkX');
-export const DefaultFeeTierOrca: Decimal = new Decimal(0.003);
+export const DefaultMintTokenB: PublicKey = new PublicKey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v');
+export const DefaultFeeTierOrca: Decimal = new Decimal(0.0005);
 export const DefaultDex: Dex = 'ORCA';
 
 export interface RebalanceMethod {
