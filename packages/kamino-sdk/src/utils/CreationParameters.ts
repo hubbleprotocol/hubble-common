10eef770371c159ab37d1712ddcf22f083a09530
Silviu Troscot <silviu.troscot10@gmail.com>
2023-05-29 19:53:15 +0300

    use usdc instead of usdh in diy vaults default config

--- packages/kamino-sdk/src/utils/CreationParameters.ts
+++ packages/kamino-sdk/src/utils/CreationParameters.ts
@@ -13,7 +13,7 @@ export const DefaultLowerPriceDifferenceBPS: number = 500;
 export const DefaultUpperPriceDifferenceBPS: number = 500;
 
 export const DefaultMintTokenA: PublicKey = new PublicKey('So11111111111111111111111111111111111111112');
-export const DefaultMintTokenB: PublicKey = new PublicKey('USDH1SM1ojwWUga67PGrgFWUHibbjqMvuMaDkRJTgkX');
+export const DefaultMintTokenB: PublicKey = new PublicKey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v');
 export const DefaultFeeTierOrca: Decimal = new Decimal(0.003);
 export const DefaultDex: Dex = 'ORCA';
 
