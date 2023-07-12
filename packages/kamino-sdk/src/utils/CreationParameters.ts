a8fb8c524d446cada38342d0f6f7b035ed76ffcb
Egor <91134968+egor-humandone@users.noreply.github.com>
2023-07-12 10:31:29 +0400

    fix: typo (#307)

--- packages/kamino-sdk/src/utils/CreationParameters.ts
+++ packages/kamino-sdk/src/utils/CreationParameters.ts
@@ -30,7 +30,7 @@ export const ManualRebalanceMethod: RebalanceMethod = {
   value: Manual.discriminator,
   descriptionShort: 'Market make with no rebalancing',
   description:
-    'Description: Liquidity is provided to a specified range width, with no rebalancing performed in any price scenario, thus no impermanent loss is via range adjustment. Asymmetric range widths are supported eg. 50% below price, and 500% above. Learn More',
+    'Liquidity is provided to a specified range width, with no rebalancing performed in any price scenario, thus no impermanent loss is via range adjustment. Asymmetric range widths are supported eg. 50% below price, and 500% above',
 };
 export const PricePercentageRebalanceMethod: RebalanceMethod = {
   label: 'Tracker',
