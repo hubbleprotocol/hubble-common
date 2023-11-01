fe3efee8516842024f168af7fb283153b6720b46
peroxy <peroxy@users.noreply.github.com>
2023-11-01 14:02:19 +0100

    Add @deprecated tag to scope-sdk mint/token methods

--- packages/scope-sdk/src/constants/ScopePairs.ts
+++ packages/scope-sdk/src/constants/ScopePairs.ts
@@ -1,3 +1,10 @@
+/**
+ * @deprecated Deprecated since version 2.2.47 - please use {@link getOraclePrices} or the respective SDK client instead.
+ * @see [hubble-sdk]{@link https://github.com/hubbleprotocol/hubble-common/blob/0512e85c5a816a557fe7feaf55981cabcd992476/packages/hubble-sdk/src/Hubble.ts#L722} getAllPrices method
+ * @see [kamino-sdk]{@link https://github.com/hubbleprotocol/hubble-common/blob/0be269d4fdb3dbadbbd8c7fcca68c6b1928d445a/packages/kamino-sdk/src/Kamino.ts#L1717} getAllPrices method
+ * @see [kamino-lending-sdk]{@link https://github.com/hubbleprotocol/kamino-lending-sdk/blob/17a48b6bb21945d2d799d31d6f0b20104e8c83ac/src/classes/market.ts#L759} getAllScopePrices method
+ * @description Scope pair config
+ */
 export const ScopePairs = [
   'SOL/USD',
   'ETH/USD',
@@ -120,4 +127,11 @@ export const ScopePairs = [
   'kSOLJITOSOLRaydium/USD',
   'kSOLMSOLRaydium/USD',
 ] as const;
+/**
+ * @deprecated Deprecated since version 2.2.47 - please use {@link getOraclePrices} or the respective SDK client instead.
+ * @see [hubble-sdk]{@link https://github.com/hubbleprotocol/hubble-common/blob/0512e85c5a816a557fe7feaf55981cabcd992476/packages/hubble-sdk/src/Hubble.ts#L722} getAllPrices method
+ * @see [kamino-sdk]{@link https://github.com/hubbleprotocol/hubble-common/blob/0be269d4fdb3dbadbbd8c7fcca68c6b1928d445a/packages/kamino-sdk/src/Kamino.ts#L1717} getAllPrices method
+ * @see [kamino-lending-sdk]{@link https://github.com/hubbleprotocol/kamino-lending-sdk/blob/17a48b6bb21945d2d799d31d6f0b20104e8c83ac/src/classes/market.ts#L759} getAllScopePrices method
+ * @description
+ */
 export type ScopePair = (typeof ScopePairs)[number];
