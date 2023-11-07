1c18b1bae0b4774f0de93dd49a07f8030f8d2c57
Silviu Troscot <silviu.troscot10@gmail.com>
2023-11-07 10:54:11 +0200

    restore new reneder token scope ID (#417)
    * add new RENDER token
    
    * remove old render token
    
    * undo the new scope ID
    
    * rename to RENDER

--- packages/scope-sdk/src/Scope.ts
+++ packages/scope-sdk/src/Scope.ts
@@ -152,8 +152,8 @@ export class Scope {
     { id: 117, pair: 'LST/SOL', name: 'LST', price: new Decimal(0), nonUsdPairId: 0 },
     { id: 118, pair: 'kSOLJITOSOLRaydium/USD', name: 'kSOLJITOSOLRaydium', price: new Decimal(0) },
     { id: 119, pair: 'kSOLMSOLRaydium/USD', name: 'kSOLMSOLRaydium', price: new Decimal(0) },
-    { id: 122, pair: 'RENDER/USD', name: 'RENDER', price: new Decimal(0) },
-    { id: 123, pair: 'RENDEREma/USD', name: 'RENDEREma', price: new Decimal(0) },
+    { id: 120, pair: 'RENDER/USD', name: 'RENDER', price: new Decimal(0) },
+    { id: 121, pair: 'RENDEREma/USD', name: 'RENDEREma', price: new Decimal(0) },
   ];
 
   /**
