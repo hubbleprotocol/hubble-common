ae7f05d01e4e5354b45b96c86b7d83d8639eedcb
Elliot Kennedy <me@elken.io>
2023-11-29 22:20:27 +0000

    Use ratio of entire pool for proportional deposit rather than 100 units as with price based deposit (#433)
    ---------
    
    Co-authored-by: Silviu Troscot <silviu.troscot10@gmail.com>

--- packages/kamino-sdk/src/Kamino.ts
+++ packages/kamino-sdk/src/Kamino.ts
@@ -5192,13 +5192,8 @@ export class Kamino {
     let bAmount = tokenBAmountUserDeposit;
 
     let [aAmounts, bAmounts] = await profiler(
-      this.calculateAmountsToBeDeposited(
-        strategy,
-        collToLamportsDecimal(new Decimal(100.0), tokenADecimals),
-        undefined,
-        profiler
-      ),
-      'C-calculateAmountsToBeDeposited',
+      this.calculateAmountsRatioToBeDeposited(strategyWithAddress, undefined, profiler),
+      'C-calculateDepositRatio',
       []
     );
 
@@ -5348,6 +5343,27 @@ export class Kamino {
       return this.calculateDepostAmountsDollarBased(strategy, tokenAAmount, tokenBAmount, profiledFunctionExecution);
     } else if (strategyState.shareCalculationMethod === PROPORTION_BASED) {
       return this.calculateDepositAmountsProportional(strategy, tokenAAmount, tokenBAmount, profiledFunctionExecution);
+    } else {
+      throw new Error('Invalid share calculation method');
+    }
+  };
+
+  /// Returns an amount of tokenA and an amount of tokenB that define the ratio of the amounts that can be deposited
+  calculateAmountsRatioToBeDeposited = async (
+    strategy: PublicKey | StrategyWithAddress,
+    holdings?: TokenAmounts,
+    profiler: ProfiledFunctionExecution = noopProfiledFunctionExecution
+  ): Promise<[Decimal, Decimal]> => {
+    const { strategy: strategyState } = await this.getStrategyStateIfNotFetched(strategy);
+    if (strategyState.shareCalculationMethod === DOLAR_BASED) {
+      return this.calculateDepostAmountsDollarBased(strategy, new Decimal(100), undefined, profiler);
+    } else if (strategyState.shareCalculationMethod === PROPORTION_BASED) {
+      let tokenHoldings = holdings;
+      if (!tokenHoldings) {
+        tokenHoldings = await profiler(this.getStrategyTokensHoldings(strategy), 'getStrategyTokensHoldings', []);
+      }
+      const { a, b } = tokenHoldings;
+      return [a, b];
     } else {
       throw new Error('Invalid share calculation method');
     }
