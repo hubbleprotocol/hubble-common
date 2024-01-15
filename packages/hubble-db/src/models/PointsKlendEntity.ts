d9923316873656d90a41137e3404440a7d82d425
peroxy <peroxy@users.noreply.github.com>
2024-01-15 11:26:35 +0100

    Update point klend entity

--- packages/hubble-db/src/models/PointsKlendEntity.ts
+++ packages/hubble-db/src/models/PointsKlendEntity.ts
@@ -2,6 +2,7 @@ export type PointsKlendEntity = {
   id: bigint;
   klend_obligation_id: number;
   klend_obligation_type_id: number;
+  klend_obligation_state_id: number;
   points_source_id: number;
   created_on: Date;
   updated_on: Date;
