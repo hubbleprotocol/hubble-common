7727c8e1aa5ac468696bc5fddcb218477dcd9445
peroxy <peroxy@users.noreply.github.com>
2024-06-10 16:12:42 +0200

    Add recent slot entity

---
+++ packages/hubble-db/src/models/RecentSlotEntity.ts
@@ -0,0 +1,5 @@
+export interface RecentSlotEntity {
+  id: number;
+  duration_in_ms: string;
+}
+export default RecentSlotEntity;
--- packages/hubble-db/src/models/index.ts
+++ packages/hubble-db/src/models/index.ts
@@ -71,6 +71,7 @@ export * from './StakingBoostEntity';
 export * from './PointsStakingEntity';
 export * from './OwnerScreeningEntity';
 export * from './accounting';
+export * from './RecentSlotEntity';
 
 export const CLUSTER_TABLE: string = 'cluster';
 export const COLLATERAL_TABLE: string = 'collateral';
@@ -149,6 +150,7 @@ export const OWNER_SCREENING_TABLE = 'owner_screening';
 export const ACCOUNTING_CONFIG_TABLE = 'accounting_config';
 export const ACCOUNTING_TRANSACTION_TABLE = 'accounting_transaction';
 export const ACCOUNTING_BALANCE_TABLE = 'accounting_balance';
+export const RECENT_SLOT_TABLE = 'recent_slot';
 
 export type RESAMPLE_FREQUENCY = 'hour' | 'day';
 export const HOURLY_FREQUENCY = 'hour';
