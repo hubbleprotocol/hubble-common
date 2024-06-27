13a2f1d80a9140286f92bda31edbfb9adb607df4
alittlezz <valentinwhite2@gmail.com>
2024-06-27 18:13:34 +0300

    Hotfix for memo

--- packages/kamino-sdk/src/constants/pubkeys.ts
+++ packages/kamino-sdk/src/constants/pubkeys.ts
@@ -7,3 +7,4 @@ export const STAGING_TOKEN_INFOS = new PublicKey('78MPQyWGwuERWoFwyryt1yhSanYc4o
 export const STAGING_KAMINO_PROGRAM_ID = new PublicKey('SKY3EZaE5p8iXG1ed4kiandK1wnwwqxmWBhjEFykaHB');
 export const STAGING_GLOBAL_CONFIG = new PublicKey('7D9KE8xxqvsSsPbpTK9DbvkYaodda1wVevPvZJbLGJ71');
 export const CONSENSUS_ID = new PublicKey('9GJmEHGom9eWo4np4L5vC6b6ri1Df2xN8KFoWixvD1Bs');
+export const MEMO_PROGRAM_ID = new PublicKey('MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr');
